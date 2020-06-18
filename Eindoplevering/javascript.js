3241ec592ccbf653cca3c12fe2ff5f567287ebed
Pepijn Kok <pepijnkok66@gmail.com>
2020-06-18 16:11:57 +0200

    comments added on css

--- Eindoplevering/javascript.js
+++ Eindoplevering/javascript.js
@@ -2,6 +2,7 @@ var allCardbacks = null;
 var columns = ['name','imgAnimated','description','howToGet',];
 // De waardes van de json die je terug wilt laten komen in de tabel
 
+
 var xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 
@@ -14,6 +15,7 @@ xhr.addEventListener("readystatechange", function () {
 	}
 });
 
+
 xhr.open("GET", "https://omgvamp-hearthstone-v1.p.rapidapi.com/cardbacks");
 xhr.setRequestHeader("x-rapidapi-host", "omgvamp-hearthstone-v1.p.rapidapi.com");
 xhr.setRequestHeader("x-rapidapi-key", "c59143bd5amshb3a51202173a9d1p19df1djsn399b91b02baa");
@@ -24,6 +26,7 @@ xhr.send();
 xhr.onload = function () {
 	//de functie wordt onload geladen
 }
+
 
 document.getElementById('search').addEventListener("input",function(){
 	let input = this.value;
@@ -38,15 +41,14 @@ document.getElementById('search').addEventListener("input",function(){
 
 	else {
 		var filteredCardbacks = allCardbacks.filter(function(cardback){
-			
 			let cardbackName = cardback['name'];
-		
 			return cardbackName.toLowerCase().includes(input.toLowerCase());
 			//loopt door alle values heen, als de name terug komt in de input is het true en laat hij hem erin. False wordt hij eruit gefilterd.
 		})
 		cards(filteredCardbacks);
 	}
 })
+
 
 function cards (cardbacks) {
 	var table = document.getElementById('cardbackTable');
@@ -99,6 +101,3 @@ function cards (cardbacks) {
 		console.log(cardbacks[i])
 	}
 }
-
-
- 
